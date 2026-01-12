078a902a1947ccc0b2c06b52a723a018d721ce96
vincentmango-wen <vincentmango.wen@gmail.com>
2026-01-12 21:42:36 +0900

    feat:エラーハンドリングを実装 close #12 (#18)
    Co-authored-by: vincent.wen <vincentmagno.wen@gamil.com>

---
+++ src/main/java/com/example/learning_exam_manager/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.learning_exam_manager.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
